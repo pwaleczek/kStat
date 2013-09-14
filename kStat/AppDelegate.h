ab93899dde5cb4626f4ab869f899a531e02d0664
Paweł Waleczek <pawel@thisismyasterisk.org>
2013-09-14 21:24:48 +0200

    Initial Commit

---
+++ kStat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kStat
+//
+//  Created by Paweł Waleczek on 9/14/13.
+//  Copyright (c) 2013 Paweł Waleczek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ kStat/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  kStat
+//
+//  Created by Paweł Waleczek on 9/14/13.
+//  Copyright (c) 2013 Paweł Waleczek. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
