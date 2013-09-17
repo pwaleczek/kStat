dd84a144ee3fb3df2517e2f5a364fa8ce44b9ce6
Paweł Waleczek <pawel@thisismyasterisk.org>
2013-09-18 00:02:32 +0200

    going for core data 'n' stuff

---
+++ kStat/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  kStat
+//
+//  Created by Paweł Waleczek on 9/17/13.
+//  Copyright (c) 2013 Paweł Waleczek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainViewController : NSViewController
+
+@end
